f475a473b9792a5f2fd58635e55dab17cb868a31
rdcotrina <roger.cotrina.c@gmail.com>
2017-03-29 19:58:48 -0500

    ....

--- vendor/bin/Registry.php
+++ vendor/bin/Registry.php
@@ -8,119 +8,122 @@ namespace Vendor;
  * @author DC
  */
 class Registry {
-    
+
     public static $get;
-    
     public static $data = [];
-    
     private static $_instancias = array();
-    
-    /** 
+
+    /**
      * Registra variables y objetos         
-     */ 
-    static private $registry = array();  
-   
+     */
+    static private $registry = array();
+
     /*
      * No se puede instanciar
      */
-    private function __construct() {}
+
+    private function __construct() {
+        
+    }
 
     /*
      * Inicia el singleton
      */
+
     public static function init() {
-        if(!self::$get){
+        if (!self::$get) {
             self::$get = Singleton::getInstancia();
         }
     }
-    
+
     /*
      * Para el registro de nuestras clases
      */
-    public static function addClass($class,$namespace){
+
+    public static function addClass($class, $namespace) {
         $c = explode('\\', $namespace);
-        $spacePlace =  $c[1];
-        
+        $spacePlace = $c[1];
+
         self::init();
-        if(!isset(self::$data[$spacePlace][$class])){
-            /*el objeto se instancia solo una vez*/
+        if (!isset(self::$data[$spacePlace][$class])) {
+            /* el objeto se instancia solo una vez */
             self::$data[$spacePlace][$class] = new $namespace;
-        }else {  
-            throw new Exception('Error: Clase <b>'.$class.'</b> ya se registro.');
-        }  
+        } else {
+            throw new Exception('Error: Clase <b>' . $class . '</b> ya se registro.');
+        }
     }
-    
-    public static function singleton($class=''){
-        if(!empty($class)){
-            if(in_array($class, self::$_instancias)){
+
+    public static function singleton($class = '') {
+        if (!empty($class)) {
+            if (in_array($class, self::$_instancias)) {
                 // throw new Exception('Error: objeto <b>'.$class.'</b> ya se instancio, para acceder hacerlo es a travez de su Controlador.');
-            }else{
+            } else {
                 self::$_instancias[$class] = $class;
             }
         }
     }
-    
-    /** 
+
+    /**
      * Método que añade objetos
      * Recibe el objeto (por referencia) y la clave
      * Devuelve un booleano para confirmar si se ha insertado
      * o si en cambio estaba duplicado.
-     */ 
-    public static function add($key, $elemento){  
-        if (!self::exists($key)) {  
-            self::$registry[$key] = $elemento;  
-            return true;  
-        } else {  
-            throw new Exception('Error: <b>'.$key.'</b> ya se registro.');
-        }  
-    }  
-   
-    /** 
+     */
+    public static function add($key, $elemento) {
+        if (!self::exists($key)) {
+            self::$registry[$key] = $elemento;
+            return true;
+        } else {
+            throw new Exception('Error: <b>' . $key . '</b> ya se registro.');
+        }
+    }
+
+    /**
      * Función que comprueba la existencia de una clave.
      * Devuelve un booleano confirmando si existe o no.
-     */ 
-    public static function exists($namespace,$class){  
+     */
+    public static function exists($namespace, $class) {
         foreach (self::$data as $key => $value) {
-            if($key == $namespace){
+            if ($key == $namespace) {
                 foreach ($value as $kkey => $vvalue) {
-                    if($class == $kkey){
+                    if ($class == $kkey) {
                         return true;
                     }
                 }
             }
         }
         return false;
-    }  
-   
-    /** 
+    }
+
+    /**
      * Función que devuelve un item dada la clave          
-     */ 
-    public static function get($key){  
-        if (self::exists($key)) { 
-            return self::$registry[$key];  
-        } else {  
-            throw new Exception('Error: Clase <b>'.$key.'</b> no existe.');
-        }  
-    }  
-   
-    /** 
+     */
+    public static function get($key) {
+        if (self::exists($key)) {
+            return self::$registry[$key];
+        } else {
+            throw new Exception('Error: Clase <b>' . $key . '</b> no existe.');
+        }
+    }
+
+    /**
      * Elimina una entrada recibiendo su clave y devuelve confirmación.
      * Si la clave no existe devuelve false.
-     */ 
-    public static function remove($name){  
-        if (self::exists($name)) {  
+     */
+    public static function remove($name) {
+        if (self::exists($name)) {
             unset(self::$registry[$name]);
-            return true;  
+            return true;
         } else {
-            throw new Exception('Error: Clase <b>'.$name.'</b> no existe.');
-        }  
-    }  
-   
-    /** 
+            throw new Exception('Error: Clase <b>' . $name . '</b> no existe.');
+        }
+    }
+
+    /**
      * Limpia el registro totalmente.         
-     */ 
-    public static function clear(){  
-        self::$registry = array();  
-    } 
-    
+     */
+    public static function clear() {
+        self::$registry = array();
+    }
+
 }
---
+++ vendor/bin/Singleton.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Vendor;
+
+/**
+ * Description of Singleton
+ *
+ * @author User
+ */
+class Singleton {
+
+    private static $_instancia;
+    private $_data;
+
+    /* singleton */
+
+    public static function getInstancia() {
+        if (!self::$_instancia instanceof self) {
+            self::$_instancia = new Singleton();
+        }
+        return self::$_instancia;
+    }
+
+    public function __set($name, $value) {
+        $this->_data[$name] = $value;
+    }
+
+    public function __get($name) {
+        if (isset($this->_data[$name])) {
+            return $this->_data[$name];
+        } else {
+            return false;
+        }
+    }
+
+    /* Evita que el objeto se pueda clonar */
+
+    public function __clone() {
+        throw new Exception('La clonación de este objeto no está permitida', E_USER_ERROR);
+    }
+
+}
